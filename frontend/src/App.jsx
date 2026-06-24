15a7bad3ad3e85c6ce8248978947d3f8c93bc563
Ronak Dutta <ronakdutta100@gmail.com>
2026-06-24 17:44:20 +0530

    Basic structural changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
+import Mainlayout from "./layouts/Mainlayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
-	return (
-		<>
-			<Toaster richColors position="top-right" />
-			{/* routes */}
-		</>
-	);
+  return (
+    <>
+      <Toaster richColors position="top-right" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mainlayout />}>
+            <Route path="home" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
