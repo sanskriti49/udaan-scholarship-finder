f6ec152140db3009f0224bcb529d4be34d41f2f8
Ronak Dutta <ronakdutta100@gmail.com>
2026-06-26 16:00:19 +0530

    Initialised elgibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,26 @@ import { Toaster } from "sonner";
 import Mainlayout from "./layouts/Mainlayout";
 import HomePage from "./pages/HomePage";
 import Support from "./pages/Support";
+import Elgibility from "./pages/Elgibility";
 
 const router = createBrowserRouter([
-	{
-		element: <Mainlayout />,
-		children: [
-			{ path: "/", element: <HomePage /> },
-			{ path: "support", element: <Support /> },
-		],
-	},
+  {
+    element: <Mainlayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "support", element: <Support /> },
+      { path: "support", element: <Support /> },
+      { path: "support", element: <Support /> },
+      { path: "eligibility", element: <Elgibility /> },
+    ],
+  },
 ]);
 
 export default function App() {
-	return (
-		<>
-			<Toaster richColors position="top-right" />
-			<RouterProvider router={router} />
-		</>
-	);
+  return (
+    <>
+      <Toaster richColors position="top-right" />
+      <RouterProvider router={router} />
+    </>
+  );
 }
