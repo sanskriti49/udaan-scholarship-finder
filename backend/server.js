3ee182010755f75b41b43daa826cbd970818f514
Ronak Dutta <ronakdutta100@gmail.com>
2026-06-28 20:33:51 +0530

    Initliased mongodb

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
